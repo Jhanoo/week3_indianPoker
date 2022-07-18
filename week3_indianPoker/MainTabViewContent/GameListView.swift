dae2cdb9feaee5bc67268e94b8ef79392fad9296
Jhanoo <jhanoo@naver.com>
2022-07-19 01:40:50 +0900

    before delete

--- week3_indianPoker/MainTabViewContent/GameListView.swift
+++ week3_indianPoker/MainTabViewContent/GameListView.swift
@@ -24,24 +24,29 @@ struct GameListView: View {
     }
 }
 
-//struct GameListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        GameListView()
-//    }
-//}
+struct GameListView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameListView()
+    }
+}
 
 
 
 struct CreateRoomButton: View {
-    @State var presentInGameView = false
+    //    @State var presentInGameView = false
     
     var title: String
     var iconName: String
     
     var body: some View {
         Button(action: {
-            presentInGameView = true
-//            SocketIOManager.shared.createRoom(hostId: "room1", user: user)
+            //            presentInGameView = true
+            NavigationView(content: {
+                NavigationLink(destination: InGameView()){
+                    Text("Enter Game")
+                }
+            })
+            //            SocketIOManager.shared.createRoom(hostId: "room1", user: user)
             
         }) {
             HStack() {
@@ -62,6 +67,8 @@ struct CreateRoomButton: View {
 
 struct RoomButtonInListView: View {
     @State private var showModal = false
+    @State private var isShowingDetailView = false
+    
     var room: Room
     var user = User(id: "test")
     
@@ -74,23 +81,26 @@ struct RoomButtonInListView: View {
             Text("\(room.title)").accentColor(.black)
         }
         .sheet(isPresented: self.$showModal) {
-            VStack{
-                Text("\(room.title)")
-                    .padding()
-                
-                Spacer()
-                ProfileImage(imageName: "Card_10")
-                Text("Name: \(room.host.name)")
-                Text("Win: \(room.host.win)\tLose: \(room.host.lose)")
-                Text("Profile: " + (room.host.profileImg))
-                
-                Button {
-//                    SocketIOManager.shared.enterRoom(hostId: "\(room.host.id)", user: user)
-                    InGameView()
-                } label: {
-                    Text("Enter game")
+            NavigationView {
+                VStack{
+                    Text("\(room.title)")
+                        .padding()
+                    
+                    Spacer()
+                    ProfileImage(imageName: "Card_10")
+                    Text("Name: \(room.host.name)")
+                    Text("Win: \(room.host.win)\tLose: \(room.host.lose)")
+                    Text("Profile: " + (room.host.profileImg))
+                    
+                    NavigationLink(destination: InGameView(), isActive: $isShowingDetailView) {}
+                    Button {
+                        //                    SocketIOManager.shared.enterRoom(hostId: "\(room.host.id)", user: user)
+                        isShowingDetailView = true
+                    } label: {
+                        Text("Enter game")
+                    }
+                    Spacer()
                 }
-                Spacer()
             }
         }
         
